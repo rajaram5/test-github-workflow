26a9e79a733163a524980bdbb3da6513a5b516d7
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2021-01-07 15:16:11 +0100

    error message

--- scripts/run.py
+++ scripts/run.py
@@ -14,8 +14,7 @@ for filename in glob.iglob(root_path+ '**/*.ttl', recursive=True):
          g = Graph()
          g.parse(filename, format="ttl")
      except Exception as e:
-         print(e)
-         logging.error("Syntaxic error reading turtle file [" +filename+"]")
-         sys.exit()
+         logging.error(e)
+         raise Exception("Syntaxic error reading turtle file [" +filename+"]")
 
 print("RDF files syntaxic validation is successful")
