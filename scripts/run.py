1bf8865af64870bf6cfd5f50ced52b51f8109e07
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2021-01-07 15:10:43 +0100

    example turtle files added with error file

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,21 @@
-x = 0
+import glob
+import logging
+from rdflib import Graph
+import sys
 
-if x > 0:
-    raise SystemExit('error in code want to exit')
-else:
-    print("RDF files syntaxic validation is done")
+# set log level
+logging.basicConfig(level=logging.INFO)
+
+root_path = "../"
+
+for filename in glob.iglob(root_path+ '**/*.ttl', recursive=True):
+     logging.info("Validating turtle file " + filename)
+     try:
+         g = Graph()
+         g.parse(filename, format="ttl")
+     except Exception as e:
+         print(e)
+         logging.error("Syntaxic error reading turtle file [" +filename+"]")
+         sys.exit()
+
+print("RDF files syntaxic validation is successful")
